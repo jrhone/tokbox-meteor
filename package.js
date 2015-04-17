f585a675d72a2eec6f75725c2ec8686fac465aeb
agufranov <albert.gufranov@gmail.com>
2015-04-17 08:49:15 +0500

    switched to newer Opentok version

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   summary: "Integrate real-time communications into your website and mobile apps using the OpenTok WebRTC API",
-  version: "2.2.8",
-  name: "carlodicelico:tokbox",
-  git: "https://github.com/carlodicelico/tokbox-meteor.git"
+  version: "2.2.12",
+  name: "agufranov:tokbox",
+  git: "https://github.com/agufranov/tokbox-meteor"
 });
 
-Npm.depends({"opentok" : "2.2.4"});
+Npm.depends({"opentok" : "2.2.5"});
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
   api.addFiles('server/index.js', 'server');
-  api.addFiles(['client/opentok.min.js', 'client/opentok-layout.min.js'], 'client');
+  api.addFiles(['client/head.html', 'client/opentok-layout.min.js'], 'client');
   api.export(['OpenTok', 'OpenTokClient'], 'server');
 });
