1b10d31b9e8977752b0f806fa452660ef0840044
Jonathan Rhone <jonathan.rhone@toptal.com>
2015-07-13 21:40:56 -0400

    only use for web, not cordova

--- package.js
+++ package.js
@@ -10,6 +10,6 @@ Npm.depends({"opentok" : "2.2.5"});
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
   api.addFiles('server/index.js', 'server');
-  api.addFiles(['client/opentok.min.js', 'client/opentok-layout.min.js'], 'client');
+  api.addFiles(['client/opentok.min.js', 'client/opentok-layout.min.js'], 'web.browser');
   api.export(['OpenTok', 'OpenTokClient'], 'server');
 });
